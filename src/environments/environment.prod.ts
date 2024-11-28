838cec16560314467bee6d265c7dfda4ad20d717
Alvitronex <marioenriquealvarado12@gmail.com>
2024-11-27 20:33:32 -0600

    change credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,10 +8,5 @@ export const environment = {
     storageBucket: "product-admin-app-2235f.appspot.com",
     messagingSenderId: "387686948449",
     appId: "1:387686948449:web:a40f2952c5e30ff1059420"
-  },
-  emailJs: {
-    serviceId: 'tu_service_id',
-    templateId: 'tu_template_id',
-    userId: 'tu_user_id'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,10 +9,5 @@ export const environment = {
     storageBucket: "product-admin-app-2235f.appspot.com",
     messagingSenderId: "387686948449",
     appId: "1:387686948449:web:a40f2952c5e30ff1059420"
-  },
-  emailJs: {
-    serviceId: 'service_3qnkqtj',
-    templateId: 'template_oitdhps',
-    userId: 'tu_user_id'
   }
 };
